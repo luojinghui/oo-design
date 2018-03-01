c378d8183d60f537062d5cdf22a62584843daca7
luojinghui <939035243@qq.com>
2018-03-01 19:56:41 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@
  * Created Date: 2018/3/1
  * Author: luojinghui
  */
+import '../assets/css/global.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,33 @@
  * Created Date: 2018/3/1
  * Author: luojinghui
  */
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
 module.exports = {
   entry: './src/main.js',
   output: {
+    path: path.resolve(__dirname,'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+    ]
+  },
+  watch: true
 };
