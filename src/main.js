d54a74340e28b759b6564557608069109d03dba7
luojinghui <939035243@qq.com>
2018-03-02 18:22:02 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@
  * Author: luojinghui
  */
 import '../assets/css/global.scss';
+
+;(function() {
+  document.getElementById("hello").addEventListener("click", function() {
+    console.log("77777");
+  }, true);
+})();
+
+window.onload=function(){
+
+};
